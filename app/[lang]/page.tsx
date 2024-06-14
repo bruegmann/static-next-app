99db2c8877c4ac6e90558c1c528444246c0a4768
lgk-bsw <lgk@patorg.de>
2024-06-14 13:08:33 +0200

    Try Image

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,5 +1,7 @@
 import { Language, getPhrase } from "@/lib/translations"
 import dynamic from "next/dynamic"
+import Image from "next/image"
+import VercelLogo from "@/lib/vercel.svg"
 
 const LanguageSwitch = dynamic(() => import("@/components/language-switch"), {
     ssr: false
@@ -18,6 +20,20 @@ export default function HomeLang({ params }: { params: { lang: Language } }) {
                 <a href="about" className="link">
                     About
                 </a>
+
+                <Image
+                    src="/next.svg"
+                    alt="Next.js Logo"
+                    width={200}
+                    height={200}
+                />
+
+                <Image
+                    src={VercelLogo}
+                    alt="Vercel Logo"
+                    width={200}
+                    height={200}
+                />
             </div>
         </>
     )
